d330b7cd10e50479297d9b50c075d0f513d53d2d
FezZ <fezzdev@gmail.com>
2020-03-25 12:00:07 +0300

    adding post widget

---
+++ module_five_task/lib/widgets/post_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:module_five_task/core/services/posts/models/post.dart';
+
+class PostView extends StatelessWidget{
+  final Post post;
+
+  PostView({Key key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+    );
+  }
+  
+}
