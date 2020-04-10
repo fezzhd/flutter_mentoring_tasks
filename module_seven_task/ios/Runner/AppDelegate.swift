a2a9a12293768579d6f02410a6d699777e7266dc
FezZ <fezzdev@gmail.com>
2020-04-10 14:43:15 +0300

    Applying maps keys

--- module_seven_task/ios/Runner/AppDelegate.swift
+++ module_seven_task/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,14 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey(loadKey())
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+    
+  private func loadKey() -> String {
+    let path = Bundle.main.path(forResource: "map_key", ofType: "txt")!
+    let text = try! String(contentsOfFile: path, encoding: String.Encoding.utf8)
+    return text
   }
 }
