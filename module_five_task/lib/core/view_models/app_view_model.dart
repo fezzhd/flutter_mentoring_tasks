167a74f47a8f9cb8097309694516a7fa476118d2
FezZ <fezzdev@gmail.com>
2020-04-30 17:10:16 +0300

    Add app view model

---
+++ module_five_task/lib/core/view_models/app_view_model.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+import 'package:module_five_task/core/services/app_services/app_localization_service.dart';
+import 'package:module_five_task/core/view_models/view_model_mixin.dart';
+import 'package:rxdart/rxdart.dart';
+
+class AppViewModel with ViewModelMixin{
+
+  AppLocalizationService _localizationService;
+  
+  BehaviorSubject<Locale> localizationSubject = BehaviorSubject<Locale>();
+  
+  AppViewModel(){
+    _localizationService = locator.get<AppLocalizationService>();
+    localizationSubject.mergeWith([_localizationService.localizationChangedSubject]);
+  }
+
+  Future setCurrentLocalization() async{
+    var currentLocale = await _localizationService.getCurrentLocale();
+    localizationSubject.add(currentLocale);
+  }
+}
--- module_five_task/lib/main.dart
+++ module_five_task/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get_it/get_it.dart';
+import 'package:module_five_task/core/services/app_services/app_localization_service.dart';
 import 'package:module_five_task/core/services/app_services/navigation_service.dart';
 import 'package:module_five_task/core/services/posts/post_service.dart';
+import 'package:module_five_task/core/view_models/app_view_model.dart';
 import 'package:module_five_task/core/view_models/post_list_view_model.dart';
 import 'package:module_five_task/core/view_models/post_view_model.dart';
 import 'package:module_five_task/routes/routes_widgets/post_list_route.dart';
@@ -12,43 +14,68 @@ import 'core/consts/routes_paths.dart' as RoutesPaths;
 import 'generated/l10n.dart';
 
 GetIt getIt = GetIt.instance;
-void main() {
+AppViewModel _appViewModel;
+Future main() async{
   _initializeBootstrapper();
+  WidgetsFlutterBinding.ensureInitialized();
+  _appViewModel = getIt.get<AppViewModel>();
+  await _appViewModel.setCurrentLocalization();
   runApp(MyApp());
 }
 
 void _initializeBootstrapper(){
   getIt.registerLazySingleton<NavigationService>(() => NavigationService());
-  getIt.registerLazySingleton<PostViewModel>(() => PostViewModel());
+  getIt.registerLazySingleton<AppLocalizationService>(() => AppLocalizationService());
   getIt.registerLazySingleton<PostService>(() => PostService());
+
+  getIt.registerFactory<AppViewModel>(() => AppViewModel());
+  getIt.registerFactory<PostViewModel>(() => PostViewModel());
   getIt.registerFactory<PostListViewModel>(() => PostListViewModel());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void dispose() {
+    _appViewModel.localizationSubject.close();
+
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: [
-        AppLocalization.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate
-      ],
-      supportedLocales: AppLocalization.delegate.supportedLocales,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        primaryTextTheme: TextTheme(
-          button: TextStyle(
-            color: Colors.black
+    return StreamBuilder<Object>(
+      stream: _appViewModel.localizationSubject,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          localizationsDelegates: [
+            AppLocalization.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate
+          ],
+          supportedLocales: AppLocalization.delegate.supportedLocales,
+          locale: snapshot.data,
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            primaryTextTheme: TextTheme(
+              button: TextStyle(
+                color: Colors.black
+              ),
+              bodyText1: TextStyle(
+                color: Colors.black
+              )
+            )
           ),
-          bodyText1: TextStyle(
-            color: Colors.black
-          )
-        )
-      ),
-      navigatorKey: getIt.get<NavigationService>().navigatorKey,
-      initialRoute: RoutesPaths.postRoute,
-      onGenerateRoute: _generateRoutes,
+          navigatorKey: getIt.get<NavigationService>().navigatorKey,
+          initialRoute: RoutesPaths.postRoute,
+          onGenerateRoute: _generateRoutes,
+        );
+      }
     );
   }
 
